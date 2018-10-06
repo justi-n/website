23e437ba132da9a3de3d7a6d1945662e0f8a9fc2
Justin Thomas <hello@justi.in>
2018-10-06 18:36:27 +0530

    Updates

---
+++ precache-manifest.fa44cc3b75f1d7c3ae179e9f6ee39f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "2812f90fe82a3e7e11d0",
+    "url": "/website/static/js/main.2812f90f.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/website/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2812f90fe82a3e7e11d0",
+    "url": "/website/static/css/main.226cc7c7.chunk.css"
+  },
+  {
+    "revision": "8ee7dce532a8efae23bb98d79e5143f1",
+    "url": "/website/index.html"
+  }
+];
